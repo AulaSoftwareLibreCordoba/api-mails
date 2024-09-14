9028751e6fe4a0f23d2ba6671144ffa05b718dd2
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-09-14 18:35:28 +0200

    error CORS solucionado en local

---
+++ src/main/java/com/spring/mail/sender/config/WebConfiguration.java
@@ -0,0 +1,14 @@
+package com.spring.mail.sender.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("http://localhost:4321").allowedMethods("*");
+    }
+}
